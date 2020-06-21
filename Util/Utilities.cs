b4d2f77cd853b135f14992370a1c560a7baedf3c
IllyaTheHath <IllyaTheHath@outlook.com>
2020-06-21 12:15:20 +0800

    fix web source timeout

--- Util/Utilities.cs
+++ Util/Utilities.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace HostsTool.Util
@@ -20,23 +21,34 @@ namespace HostsTool.Util
 
         public static async Task<String> GetStringAsync(String url)
         {
-            String result = String.Empty;
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Timeout = 15;
-
             try
             {
-                using var response = (HttpWebResponse)await request.GetResponseAsync().ConfigureAwait(false);
-                using var stream = response.GetResponseStream();
-                using var reader = new StreamReader(stream);
-                result = reader.ReadToEnd();
+                using HttpClient client = new HttpClient();
+                client.Timeout = TimeSpan.FromSeconds(15);
+                return await client.GetStringAsync(url);
             }
             catch (Exception)
             {
                 throw;
             }
 
-            return result;
+            //String result = String.Empty;
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            //request.Timeout = 15000;
+
+            //try
+            //{
+            //    using var response = (HttpWebResponse)await request.GetResponseAsync().ConfigureAwait(false);
+            //    using var stream = response.GetResponseStream();
+            //    using var reader = new StreamReader(stream);
+            //    result = reader.ReadToEnd();
+            //}
+            //catch (Exception ex)
+            //{
+            //    throw;
+            //}
+
+            //return result;
         }
 
         public static void MixHosts(ref String hosts, String data, String title)
